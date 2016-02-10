0bcbe55be8e1cd84f337fdd503f7c8fe755ecdc0
Luke Pettway <luke@pancakes.local>
2016-02-09 21:43:26 -0500

    nothing to see here :P

--- admin/js/wp-rest-api-test-bed-admin.js
+++ admin/js/wp-rest-api-test-bed-admin.js
@@ -1,47 +1,24 @@
 (function( $ , _ ) {
 	'use strict';
-
-	/**
-	 * All of the code for your admin-facing JavaScript source
-	 * should reside in this file.
-	 *
-	 * Note: It has been assumed you will write jQuery code here, so the
-	 * $ function reference has been prepared for usage within the scope
-	 * of this function.
-	 *
-	 * This enables you to define handlers, for when the DOM is ready:
-	 *
-	 * $(function() {
-	 *
-	 * });
-	 *
-	 * When the window is loaded:
-	 *
-	 * $( window ).load(function() {
-	 *
-	 * });
-	 *
-	 * ...and/or other possibilities.
-	 *
-	 * Ideally, it is not considered best practise to attach more than a
-	 * single DOM-ready or window-load handler for a particular page.
-	 * Although scripts in the WordPress core, Plugins and Themes may be
-	 * practising this, we should strive to set a better example in our own work.
-	 */
+	 //Thanks Rachel Baker for giving me a starting point!
 
 	 var $restContentBtn = $( '.load-rest-content' );
 	 var $restRequestStringInput = $( '#rest-request-string' );
 
 	 $restContentBtn.on( 'click', function(){
- 		 //Thanks Rachel Baker for giving me a starting point!
+ 		 
 		var apiEndpointData = $(this).attr('href'), 
 		$responseContainerFormated = $( '#js-data-formated' ),
 		$responseContainerJSON =  $( '#js-data-json' ),
 		$viewRequestEndpoint = $('#viewRequestEndpoint'),
-
 		tmpl = '<article id="post-<%= id %>"><h1><%= title %></h1><%= content %></article>';
 
 	 	$.get( '/wp-json/wp/v2/' + apiEndpointData , function( data ) {
+
+	 		$responseContainerFormated.empty();
+	 		$restRequestStringInput.val( '/wp-json/wp/v2/' + apiEndpointData ) ;
+ 			$viewRequestEndpoint.attr("href", '/wp-json/wp/v2/' + apiEndpointData )
+
 	 		for ( var key in data ) {
 	 			var output = {
 	 				id : data[ key ].id,
@@ -49,18 +26,17 @@
 	 				content : data[ key ].content.rendered,
 	 				},
 	 				$template = $( _.template( tmpl , output ) );
-
- 				$restRequestStringInput.empty().val( '/wp-json/wp/v2/' + apiEndpointData ) ;
-				$responseContainerFormated.empty().append( $template );
-				$viewRequestEndpoint.attr("href", '/wp-json/wp/v2/' + apiEndpointData )
+				$responseContainerFormated.append( $template );
 	 		}
 
 	 		var dataString = JSON.stringify(data);
 
 	 		$responseContainerJSON.text( dataString );
+	 		
 	 	});
 
 	 	return false;
+
 	 });
 
 })( jQuery , _ );
