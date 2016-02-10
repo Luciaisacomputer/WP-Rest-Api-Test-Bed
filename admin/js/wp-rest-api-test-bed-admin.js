327aa57c7990cd8e632ab3a2ec4047ad533c4651
Luke Pettway <luke@pancakes.local>
2016-02-09 21:54:49 -0500

    Moved a few things around to make code easier to read, magic

--- admin/js/wp-rest-api-test-bed-admin.js
+++ admin/js/wp-rest-api-test-bed-admin.js
@@ -1,42 +1,45 @@
 (function( $ , _ ) {
 	'use strict';
-	 //Thanks Rachel Baker for giving me a starting point!
+	//Thanks Rachel Baker for giving me a starting point!
 
-	 var $restContentBtn = $( '.load-rest-content' );
-	 var $restRequestStringInput = $( '#rest-request-string' );
-
-	 $restContentBtn.on( 'click', function(){
- 		 
-		var apiEndpointData = $(this).attr('href'), 
+	//Dom caching
+	var $restContentBtn = $( '.load-rest-content' ),
+		$restRequestStringInput = $( '#rest-request-string' ),
 		$responseContainerFormated = $( '#js-data-formated' ),
 		$responseContainerJSON =  $( '#js-data-json' ),
 		$viewRequestEndpoint = $('#viewRequestEndpoint'),
-		tmpl = '<article id="post-<%= id %>"><h1><%= title %></h1><%= content %></article>';
 
-	 	$.get( '/wp-json/wp/v2/' + apiEndpointData , function( data ) {
+	$restContentBtn.on( 'click', function(){
+		 
+		var apiEndpointData = $(this).attr('href'), 
+			tmpl = '<article id="post-<%= id %>"><h1><%= title %></h1><%= content %></article>';
 
-	 		$responseContainerFormated.empty();
-	 		$restRequestStringInput.val( '/wp-json/wp/v2/' + apiEndpointData ) ;
- 			$viewRequestEndpoint.attr("href", '/wp-json/wp/v2/' + apiEndpointData )
+		$.get( '/wp-json/wp/v2/' + apiEndpointData , function( data ) {
 
-	 		for ( var key in data ) {
-	 			var output = {
-	 				id : data[ key ].id,
-	 				title : data[ key ].title.rendered,
-	 				content : data[ key ].content.rendered,
-	 				},
-	 				$template = $( _.template( tmpl , output ) );
+			$responseContainerFormated.empty();
+			$restRequestStringInput.val( '/wp-json/wp/v2/' + apiEndpointData ) ;
+			$viewRequestEndpoint.attr("href", '/wp-json/wp/v2/' + apiEndpointData )
+
+			for ( var key in data ) {
+
+				var output = {
+						id : data[ key ].id,
+						title : data[ key ].title.rendered,
+						content : data[ key ].content.rendered,
+					},
+					$template = $( _.template( tmpl , output ) );
+
 				$responseContainerFormated.append( $template );
-	 		}
+			}
 
-	 		var dataString = JSON.stringify(data);
+			var dataString = JSON.stringify(data);
 
-	 		$responseContainerJSON.text( dataString );
-	 		
-	 	});
+			$responseContainerJSON.text( dataString );
 
-	 	return false;
+		});
 
-	 });
+		return false;
+
+	});
 
 })( jQuery , _ );
